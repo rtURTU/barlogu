9255dd5f51e4f3b48448d727bd5d71a2214afa3f
ad5915 <dan.alex97@yahoo.com>
2016-07-21 22:18:31 +0300

    Implemented problem fether.

--- scripts/cf.py
+++ scripts/cf.py
@@ -3,7 +3,7 @@
 import requests
 import json
 
-def get_problems(user):
+def cf_problem_map(user):
 	model = "http://codeforces.com/api/user.status?handle=" + user + "&from=1"
 	r = requests.get(model)
 
@@ -16,15 +16,53 @@ def get_problems(user):
 		
 		if str(obj["verdict"]) =="OK":
 			problem_map[(ctst, problem)] = 1
-	return len(problem_map)
+	return problem_map
 
-users = ["dyskode", "danalex"] 
-for user in users:
-	print "<p>"
-	print get_problems(user)
-	print "</p>"
+def get_by_patterns(user, model, start, stop, check, host):
+	r = requests.get(model)
+	content = str(r.content)
+
+	l = content.find(start)
+	r = content.find(stop, l + 1)
+	p = content.find(check, l + 1)
+
+	problem_list = []
+
+	while p < r:
+		problem = ""
+		while content[p] != '\"':
+			problem += content[p]
+			p += 1
+		if p > r:
+			break
+		p = content.find(check, p)
+		problem_list.append(host + problem)
+	return problem_list
+
+def ia_problem_map(user):
+	model = "http://www.infoarena.ro/utilizator/"+ user +"?action=stats"
+	return get_by_patterns(
+		user,
+		model, 
+		"<h3>Probleme rezolvate</h3>", 
+		"<h3>Probleme incercate</h3>", 
+		"problema",
+		"http://www.infoarena.ro/"
+	) 
+
+def pb_problem_map(user):
+	model = "http://www.pbinfo.ro/?pagina=profil&subpagina=probleme&user=" + user
+	return get_by_patterns(
+		user,
+		model, 
+		"probleme rezolvate", 
+		"nerezolvate", 
+		"?pagina=judge-board&id_problema=",
+		"http://www.pbinfo.ro/"
+	) 
 
 def main():
-    return 0 
+	pass
+
 if __name__ == "__main__":
-    x = main()
+	pass	
