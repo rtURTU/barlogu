06557314c38ce5390c7eed76d2fd75185d8948c7
Razvan Turtu <razvan.turtu@yahoo.com>
2016-07-26 22:08:14 -0700

    modified style, fixing calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,23 @@
-function MakeCalendar( divId ){
+function MakeCalendar( divId , year , month ){
     var context = document.getElementById( divId );
     var dayHash = [ 7 , 1 , 2 , 3 , 4 , 5 , 6 ];
     var zile = [ "" , "Luni" , "Marti" , "Miercuri" , "Joi" , "vineri" , "Sambata" , "Dimineca" ];
-    var date = new Date();
-    date.setMonth( date.getMonth() + 2 );
+    var date = new Date( year , month );
+    
+    context.innerHTML = "";
     
     var calendarHeader = document.createElement( "div" );
     calendarHeader.classList.add( "row" );
     calendarHeader.classList.add( "col-xs-12" );
+    
+    for( var i = 1 ; i <= 7 ; i++ ){
+        var newTile = document.createElement( "div" );
+        newTile.classList.add( "odd-color" );
+        newTile.classList.add( "calendar-tile" );
+        newTile.classList.add( "text-center" );
+        newTile.innerHTML = zile[ i ];
+        calendarHeader.appendChild( newTile );
+    }
     
     var firstDay = new Date( date.getFullYear() , date.getMonth() , 1 );
     var lastDay = new Date( date.getFullYear() , date.getMonth() + 1 , 0 );
@@ -19,19 +29,23 @@ function MakeCalendar( divId ){
         var newTile = document.createElement( "div" );
         newTile.classList.add( "odd-color" );
         newTile.classList.add( "calendar-tile" );
+        newTile.classList.add( "text-center" );
         newTile.innerHTML = lastMonth.getDate();
         lastMonth.setDate( lastMonth.getDate() + 1 );
         calendarHeader.appendChild( newTile );
     }
-    
+    var ok = 1;
     while( firstDay <= lastDay ){
         var newTile = document.createElement( "div" );
         newTile.classList.add( "even-color" );
         newTile.classList.add( "calendar-tile" );
-        var tileText = document.createElement( "p" );
-        tileText = firstDay.getDate();
-        newTile.innerHTML = firstDay.getDate();
+        newTile.classList.add( "text-center" );
+        var tileText = document.createElement( "a" );
+        tileText.innerHTML = firstDay.getDate();
+        newTile.appendChild( tileText );
         calendarHeader.appendChild( newTile );
+        newTile.onclick = function(){ DisplayPopUp( year , month , tileText.innerHTML ); };
+        ok++;
         firstDay.setDate( firstDay.getDate() + 1 );
     }
     
@@ -39,10 +53,16 @@ function MakeCalendar( divId ){
         var newTile = document.createElement( "div" );
         newTile.classList.add( "odd-color" );
         newTile.classList.add( "calendar-tile" );
+        newTile.classList.add( "text-center" );
         newTile.innerHTML = i - dayHash[ lastDay.getDay() ];
         calendarHeader.appendChild( newTile );
     }
     
     context.appendChild( calendarHeader );
-    
+}
+
+function DisplayPopUp( year , month , day ){
+    console.log( year );
+    console.log( month );
+    console.log( day );
 }
