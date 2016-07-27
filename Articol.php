aea0a3eafbecfd167e108034a040d74b280aae69
GreenerC <tolan_cristian2000@yahoo.com>
2016-07-27 12:29:58 +0300

    Modificare minora la Articol.php; Am adaugat Games.php

---
+++ Articol.php
@@ -0,0 +1,64 @@
+<html>
+    
+    <head>
+        <title>Home</title>
+        <link rel = "stylesheet" href = "css/bootstrap.min.css">
+        <link rel = "stylesheet" href = "css/style.css">
+        <link rel = "stylesheet" href = "styleG.css">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:500,400' rel='stylesheet' type='text/css'>
+        <script src = "js/script.js"></script>
+    </head>
+    
+    <body>
+        <div class = "col-lg-1 float-left">
+            
+        </div>
+        <div class = "col-lg-9 float-left">
+            <div class = "col-lg-1 float-left">
+                
+            </div>
+            <div class = "col-lg-10 float-left">
+                <div class = "col-lg-12 article-image-div">
+                    <div class = "col-lg-2 float-left">
+                        
+                    </div>
+                    <div class = "col-lg-8 float-left">
+                        <img class = "fit-div" src = "http://indianapublicmedia.org/amomentofscience/files/2012/10/217_bearsscratch-940x626.jpg">
+                    </div>
+                    <div class = "col-lg-2 float-left">
+                        
+                    </div>
+                </div>
+                
+                <div class = "col-lg-12 float-left title-div">
+                    <div class = "col-lg-3 float-left">
+                    
+                    </div>
+                    <div class = "col-lg-6 float-left article-title">
+                        <h1 class = "text-center">Titlu</h1>
+                    </div>
+                    <div class = "col-lg-3 float-left">
+                        
+                    </div>
+                </div>
+                
+                <div class = "col-lg-12 float-left content-div">
+                    <div class = "col-lg-1 float-left">
+                    
+                    </div>
+                    <div class = "col-lg-10 float-left article-content">
+                        <p> Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare.Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare.Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare.Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare. Acesta este un paragraf, si trebuie tratat ca atare.</p>
+                    </div>
+                    <div class = "col-lg-1 float-left">
+                    
+                    </div>
+                </div>
+                
+            </div>
+            <div class = "col-lg-1 float-left">
+            
+            </div>
+        </div>
+    </body>
+    
+</html>
